aadf7f9634aab80ebfc7c3acddf19181c1a0ec27
zjximfg <13752560882@users.noreply.github.com>
2020-07-26 16:00:44 +0800

    添加返回当前用户的企业连接信息

---
+++ imes_warehouse/src/main/java/cn/jianing/imes/warehouse/service/WorkAreaService.java
@@ -0,0 +1,20 @@
+package cn.jianing.imes.warehouse.service;
+
+import cn.jianing.imes.domain.warehouse.WorkArea;
+import com.github.pagehelper.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public interface WorkAreaService {
+    Page<WorkArea> getWorkAreaPageByCompanyId(int current, int pageSize, String companyId, Map<String, Object> map);
+
+    WorkArea getWorkAreaById(String id);
+
+    void deleteWorkAreaById(String id);
+
+    void updateWorkAreaById(WorkArea workArea);
+
+    void insertWorkArea(WorkArea workArea);
+}
